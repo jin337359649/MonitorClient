0624843b9dee9555d6f613c63fb19067cb7399b7
jin337359649 <337359649@qq.com>
2018-12-14 10:50:29 +0800

    2

--- src/api/devices.js
+++ src/api/devices.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const hoistUrl = 'http://localhost:9111/api'
+const hoistUrl = 'http://61.147.108.47:9111/api'
 
 export function getList(params) {
   return axios.get(`${hoistUrl}/Devices/QueryDevices`, { params: params })
